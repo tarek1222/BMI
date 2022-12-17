c891490f75c4dd43b37603cc033ad857b8830e00
tarek1222 <113442888+tarek1222@users.noreply.github.com>
2022-12-17 19:33:22 +0200

    Update bmi.js

--- bmi.js
+++ bmi.js
@@ -9,37 +9,46 @@
 // be available in developers tools console in browser).
 "use strict";
 
-
 // TODO:
 // (a) global variables for storing min and max BMI
-
-let minBMI;
-let maxBMI;
+let minBMI = 0.0
+let maxBMI = 0.0
 
 /**
  * Function init() that is called when the page bmi.html is loaded.
  */  
 function init() {
-    // TODO:
-    // (a) add local variables for min and max and
-    let minLocal;
-    let maxLocal;
+   
+    
     // (b) initialize all variables by getting them from the local storage
     //     (they will be null, if they were not in the local storage)
-    let height = 0;
-    let weight = 0;
-    
+    let height = localStorage.getItem('height');
+    let weight = localStorage.getItem('weight');
+    let min = localStorage.getItem('minBMI');
+    let max = localStorage.getItem('maxBMI');
+   
 
-    // TODO:
-    // (b) replace with if-else to check if data was in local storage
+    
+         // if-else to check if data was in local storage
         // if yes, set data on the page and global variables
         // to the saved values,
         // otherwise, set date on the page to default values
-    document.getElementById('no').checked = 'true';
+        if(height != null && weight != null 
+            && minBMI != null && maxBMI != null) {
+                document.getElementById('height').value = height;
+                document.getElementById('weight').value = weight;
+                document.getElementById('minAndMaxBMI').innerHTML = 
+                '<P>Min BMI:' + min + '</P><P>Max BMI:  ' + max + '</P>';
+            document.getElementById('yes').checked = 'true';
+            minBMI = min;
+            maxBMI = max;
 
+        } else {
+            document.getElementById('no').checked = 'true';
+        }
     // call function calculateBMI with height and weight
     // gotten from the input fields of the page
-    calculateBMI(document.getElementById('height').value,
+     calculateBMI(document.getElementById('height').value,
         document.getElementById('weight').value);
 }
 
@@ -60,6 +69,12 @@ function calculateBMI(height, weight) {
     // TODO:
     // (a) check if this is the first time the BMI was calculated and
     //     if it is, update the BMI to global min and max and on the page
+    if (minBMI == 0.0 && maxBMI == 0.0) {
+        minBMI = bmi;
+        maxBMI = bmi;
+        document.getElementById('minAndMaxBMI').innerHTML = 
+        '<P>Min BMI:' + minBMI + '</P><P>Max BMI:  ' + maxBMI + '</P>';
+    }
 
     // set the calculated BMI to page
     document.getElementById('calculatedBMI').innerHTML =
@@ -69,6 +84,17 @@ function calculateBMI(height, weight) {
     // (a) check if the calculated BMI is new min or max and
     //     if it is, update it on the page and on the global variable
     //     accordingly
+    if (bmi > maxBMI) {
+        maxBMI = bmi
+        document.getElementById('minAndMaxBMI').innerHTML = 
+        '<P>Min BMI:' + minBMI + '</P><P>Max BMI:  ' + maxBMI + '</P>';
+
+    } else if (bmi < minBMI) {
+        minBMI = bmi
+        document.getElementById('minAndMaxBMI').innerHTML = 
+        '<P>Min BMI:' + minBMI + '</P><P>Max BMI:  ' + maxBMI + '</P>';
+
+    }
     
     // check if the radio button for saving is set to "Yes" and
     // if it is, call function store(setting) with parameter true
@@ -86,4 +112,14 @@ function store(setting){
     // TODO:
     // (b) if setting is true, save data to local storage,
     //     otherwise, remove all data from local storage
+
+    if (setting == true) {
+        localStorage.setItem('height', document.getElementById('height').value); 
+        localStorage.setItem('weight', document.getElementById('weight').value); 
+        localStorage.setItem('minBMI', minBMI); 
+        localStorage.setItem('maxBMI', maxBMI);  
+         
+    } else {
+        localStorage.clear();
+    }
 }
